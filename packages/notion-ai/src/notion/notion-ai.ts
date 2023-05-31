05fe8411d4502bfb0c28b78ba8aeac6b2744eda3
seonglae <sungle3737@gmail.com>
2023-05-31 22:47:36 +0900

    refactor: notion-ai generic type inheritance

--- packages/notion-ai/src/notion/notion-ai.ts
+++ packages/notion-ai/src/notion/notion-ai.ts
@@ -2,9 +2,13 @@ import type { BaseLLM } from 'langchain/llms/base'
 import type { BaseChain } from 'langchain/chains'
 import type { VectorStore, VectorStoreRetriever } from 'langchain/vectorstores/base'
 
-export abstract class NotionAI {
-  llm: BaseLLM
-  chain: BaseChain
-  vectorStore: VectorStore
-  retriever: VectorStoreRetriever
+export abstract class NotionAI<
+  LLM extends BaseLLM = BaseLLM,
+  Chain extends BaseChain = BaseChain,
+  Store extends VectorStore = VectorStore
+> {
+  llm: LLM
+  chain: Chain
+  vectorStore: Store
+  retriever: VectorStoreRetriever<Store>
 }
--- packages/notion-ai/src/notion/notion-qa.ts
+++ packages/notion-ai/src/notion/notion-qa.ts
@@ -8,7 +8,7 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 import { NotionAI } from './notion-ai'
 
-export class NotionQA extends NotionAI {
+export class NotionQA extends NotionAI<OpenAI, RetrievalQAChain, HNSWLib> {
   declare vectorStore: HNSWLib
 
   constructor({ temperature, openAIApiKey }: { temperature?: number; openAIApiKey?: string } = {}) {
@@ -19,11 +19,11 @@ export class NotionQA extends NotionAI {
 
   async load(path: string) {
     this.vectorStore = await HNSWLib.load(path, new OpenAIEmbeddings())
-    this.retriever = this.vectorStore.asRetriever()
+    this.retriever = this.vectorStore.asRetriever(1)
     this.chain = RetrievalQAChain.fromLLM(this.llm, this.retriever)
   }
 
-  async ingest(id: string) {
+  async ingest(id: string, path: string = 'store.pkl') {
     const notionClient = new NotionAPI()
     const page = notionClient.getPage(id)
     const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 10, chunkOverlap: 1 })
@@ -33,13 +33,14 @@ export class NotionQA extends NotionAI {
       splitted.map(doc => doc.metadata),
       new OpenAIEmbeddings()
     )
-    this.retriever = this.vectorStore.asRetriever()
+    this.retriever = this.vectorStore.asRetriever(1)
     this.chain = RetrievalQAChain.fromLLM(this.llm, this.retriever)
+    this.vectorStore.save(path)
     return id
   }
 
   async ask(question: string) {
     if (!this.chain) throw new Error('NotionQA not loaded')
-    return this.chain.call({ question: question })
+    return this.chain.call({ query: question })
   }
 }
